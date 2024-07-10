feab93b98f4bc708ea1390862c4ff703808bebdf
user <mail@ex.com>
2024-07-10 16:16:15 -0300

    Operação em radio button

--- eventos-js/scripts/calculadora.js
+++ eventos-js/scripts/calculadora.js
@@ -43,6 +43,60 @@ function calculate() {
   // console.log(inputOp1);
 }
 
-document.querySelector('input[name=operacao]').addEventListener('keyup', () => {
-    calculate();
+function calculateByRadioButton() {
+    const inputOp1 = document.querySelector("input[name=op1]");
+    const inputOp2 = document.querySelector("input[name=op2]");
+    
+    const inputOperacao = document.querySelectorAll("input[name=operacao]");
+
+    let operacao = '';
+    inputOperacao.forEach( (e) => {
+        if(e.checked === true) operacao = e.value;
+    })
+  
+    let resultado = 0;
+  
+    const value1 = Number(inputOp1.value);
+    const value2 = Number(inputOp2.value);
+  
+    if(operacao !== ''){
+      switch (operacao) {
+          case "+":
+          resultado = value1 + value2;
+          break;
+  
+          case "-":
+          resultado = value1 - value2;
+          break;
+  
+          case "*":
+          resultado = value1 * value2;
+          break;
+  
+          case "/":
+          resultado = value1 / value2;
+          break;
+  
+          default: resultado = undefined;
+      }
+  
+      document.querySelector('#resultado').innerText = `
+          Resultado: ${resultado}
+      `;
+  
+      if(resultado == undefined){
+          document.querySelector('#resultado').innerText = 'Erro';
+      }
+      } else{
+          document.querySelector('#resultado').innerText = 'Aguardando operação...';
+      }
+  
+    // console.log(inputOp1);
+  }
+
+document.querySelectorAll('input[name=operacao]').forEach(
+    (e) => {
+        e.addEventListener('click', () => {
+            calculateByRadioButton();
+        })
 })
